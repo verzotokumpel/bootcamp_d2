36eaddf7b6eee513c5c9dde7f0f1d74a01024b77
verzotokumpel <verzo.of@gmail.com>
2024-06-26 17:38:15 +0200

    added new feature

--- src/day2_backend/src/lib.rs
+++ src/day2_backend/src/lib.rs
@@ -16,7 +16,24 @@ fn add_blog(blog: String) {
 
 #[ic_cdk::query]
 fn read_blogs() -> Vec<String> {
+    BLOGS.with(|blogs| blogs.borrow().clone())
+}
+
+#[ic_cdk::update]
+fn delete_blog(id: usize) {
     BLOGS.with(|blogs| {
-        blogs.borrow().clone()
-    })
+        blogs.borrow_mut().remove(id);
+    });
+}
+
+#[ic_cdk::update]
+fn edit_blog(id: usize, new_blog: String) {
+    BLOGS.with(|blogs| {
+        let mut binding_blogs = blogs.borrow_mut();
+        let blog = binding_blogs.get_mut(id);
+        let old_blog = blog.unwrap();
+        if old_blog != &new_blog {
+            *old_blog = new_blog;
+        }
+    });
 }
